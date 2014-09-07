9bde7eecb773061318fba2daeed49681b00ec67b
Fred Eisele <phreed@gmail.com>
2014-09-07 13:35:06 -0500

    Service the calls first
    This overcomes the problem with forgetting the running heading.

--- hw1/src/matlab/handle_event.m
+++ hw1/src/matlab/handle_event.m
@@ -2,13 +2,15 @@
 function [h1,f1, u1,d1,i1] = handle_event(h0,f0,u0,d0,i0)
 %SERVICE_CALLS Service the elevator calls
 %
-[h1] = update_heading(h0,f0,u0,d0,i0);
-
-% visit the next floor
-f1 = f0 + h1;
 
 % Service the floor by clearing its calls.
-[u1,d1,i1] = service_calls(h1,f1,u0,d0,i0);
+[u1,d1,i1] = service_calls(h0,f0,u0,d0,i0);
+
+% Update the heading based on the state
+[h1] = update_heading(h0,f0,u0,d0,i0);
+
+% visit the next floor, where next is identified by the heading
+f1 = f0 + h1;
 
 end
 
